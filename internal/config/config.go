78b6302d104f2ab2181804908582e887559ea2a7
knnedy <kennymwendwadev@gmail.com>
2026-06-10 11:40:49 +0300

    feat(config): hardcoded local config with port 7080

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,34 +1,14 @@
 package config
 
-import (
-	"os"
-	"strconv"
-)
-
 type Config struct {
 	Port string
 	Host string
 }
 
 func Load() Config {
-	port := os.Getenv("NEXUSQL_PORT")
-	if port == "" {
-		port = "7080"
-	}
-
-	host := os.Getenv("NEXUSQL_HOST")
-	if host == "" {
-		host = "127.0.0.1"
-	}
-
-	// validate port is a number
-	if _, err := strconv.Atoi(port); err != nil {
-		port = "7080"
-	}
-
 	return Config{
-		Port: port,
-		Host: host,
+		Port: "7080",
+		Host: "127.0.0.1",
 	}
 }
 
