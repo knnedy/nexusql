3296ae977e377c637064331379e1a9bbd5572f88
knnedy <kennymwendwadev@gmail.com>
2026-07-09 21:21:58 +0300

    feat(cmd): register postgres provider via blank import

--- cmd/nexusql/main.go
+++ cmd/nexusql/main.go
@@ -14,6 +14,7 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/knnedy/nexusql/internal/api"
 	"github.com/knnedy/nexusql/internal/config"
+	_ "github.com/knnedy/nexusql/internal/db/postgres"
 	"github.com/knnedy/nexusql/internal/projects"
 	"github.com/knnedy/nexusql/internal/session"
 )
